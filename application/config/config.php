29501c60197ea4a2b9a86fb6bf03f80df8198232
Ashar Fuadi <fushar@gmail.com>
2015-07-11 01:12:39 +0700

    Set sess_save_path in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -363,7 +363,7 @@ $config['sess_expiration']		= 7200;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
-$config['sess_save_path'] 		= NULL;
+$config['sess_save_path'] 		= BASEPATH . 'sessions';
 $config['sess_time_to_update']	= 300;
 
 /*
